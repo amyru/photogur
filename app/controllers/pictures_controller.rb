0b44cf049fdf06761259ea552d64d8d1abe50871
Amy Rudolph <amy.rudolph@live.co.uk>
2014-11-04 16:09:56 -0500

    created pictures to show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,22 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = [
+			{
+				:title =>"the old church on the coast of the White sea",
+				:artist => "Sergey Ershov",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+			},
+			{
+				:title => "Sea Power",
+				:artist => "Stephen Scullion",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+			},
+			{
+				:title => "into the Poppies",
+				:artist => "John Wilhelm",
+				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+			}
+		]
 
 	end
 end
