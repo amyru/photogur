6de82b689a11f542f3c2cd383f27447935bb7d2b
Amy Rudolph <amy.rudolph@live.co.uk>
2014-11-04 17:18:42 -0500

    moving @pictures in controller to the database to db:seed

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,44 +1,10 @@
 class PicturesController < ApplicationController
 	def index
-		@pictures = [
-			{
-				:title =>"the old church on the coast of the White sea",
-				:artist => "Sergey Ershov",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-			},
-			{
-				:title => "Sea Power",
-				:artist => "Stephen Scullion",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-			},
-			{
-				:title => "into the Poppies",
-				:artist => "John Wilhelm",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-			}
-		]
-
+		@pictures = Picture.all
 	end
 
 	def show
-		@pictures = [
-				{
-					:title =>"the old church on the coast of the White sea",
-					:artist => "Sergey Ershov",
-					:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-				},
-				{
-					:title => "Sea Power",
-					:artist => "Stephen Scullion",
-					:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-				},
-				{
-					:title => "into the Poppies",
-					:artist => "John Wilhelm",
-					:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-				}
-			]
-			@picture = @pictures[params[:id].to_i]
+		@picture = Picture.find(params[:id]) #why do we not need to integer here?
 	end 
 
 	def new
