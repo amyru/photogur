d48217e64dce249b4c4cf2bf33d3f61e23e52cd5
Amy Rudolph <amy.rudolph@live.co.uk>
2014-11-04 18:08:14 -0500

    added edit show and route and added link tog to edit on show page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,8 +16,21 @@ class PicturesController < ApplicationController
 		if @picture.save
 			redirect_to pictures_url
 		else
-
 			render :new
+		end
+	end
+
+	def edit
+		@picture = Picture.find(params[:id])
+	end
+
+	def update
+		@picture = Picture.find(params[:id])
+
+		if @picture.update_attributes(picture_params)
+			redirect_to "/pictures/#{@picture.id}"
+		else
+			render :edit
 		end
 	end
 
