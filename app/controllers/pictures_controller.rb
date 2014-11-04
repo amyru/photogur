911664bc423d50fdb8e25a66cadeaabee3abf100
Amy Rudolph <amy.rudolph@live.co.uk>
2014-11-04 16:23:45 -0500

    created a show page for each photo

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,4 +19,26 @@ class PicturesController < ApplicationController
 		]
 
 	end
+
+	def show
+		@pictures = [
+				{
+					:title =>"the old church on the coast of the White sea",
+					:artist => "Sergey Ershov",
+					:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+				},
+				{
+					:title => "Sea Power",
+					:artist => "Stephen Scullion",
+					:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+				},
+				{
+					:title => "into the Poppies",
+					:artist => "John Wilhelm",
+					:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+				}
+			]
+			@picture = @pictures[params[:id].to_i]
+	end 
+
 end
